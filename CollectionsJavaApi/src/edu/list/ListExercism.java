b3576076e160aaebca1215a3d870dfb74d7e8fce
Felicio <victor_felicio@hotmail.com>
2024-08-20 01:02:27 -0300

    feat: create List obj

---
+++ CollectionsJavaApi/src/edu/list/ListExercism.java
@@ -0,0 +1,17 @@
+package edu.list;
+
+public class ListExercism {
+    public static void main(String[] args) {
+        ListaTarefas listMain = new ListaTarefas();
+
+        listMain.adicionarTarefa("jogar futebol");
+        listMain.adicionarTarefa("estudar java");
+        listMain.adicionarTarefa("descansar depois");
+
+        System.out.println(listMain.obterTotalDeTarefas());
+
+        listMain.obterDescricaoTarefas();
+        listMain.removerTarefa("jogar futebol");
+        listMain.obterDescricaoTarefas();
+    }
+}
---
+++ CollectionsJavaApi/src/edu/list/ListaTarefas.java
@@ -0,0 +1,37 @@
+package edu.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+
+    private List<Tarefa> listaDeTarefas;
+
+    public ListaTarefas() {
+        this.listaDeTarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        this.listaDeTarefas.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa tarefa : this.listaDeTarefas) {
+            if (tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(tarefa);
+            }
+        }
+        this.listaDeTarefas.removeAll(tarefasParaRemover);
+    }
+
+    public int obterTotalDeTarefas() {
+        return this.listaDeTarefas.size();
+    }
+
+    public void obterDescricaoTarefas() {
+
+        System.out.println(this.listaDeTarefas);
+    }
+
+}
